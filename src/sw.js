52abcd29203647c52108e4f8eb389fc21f1e65f6
Karolina Skibińska <karolina.skibinska@airhelp.com>
2019-01-19 21:20:45 +0100

    Implement service worker

---
+++ src/sw.js
@@ -0,0 +1,44 @@
+// ESLint global registration
+/* global serviceWorkerOption: false */
+
+const cacheName = 'kanban-cache';
+const isExcluded = f => /hot-update|sockjs/.test(f);
+
+const filesToCache = [
+  ...serviceWorkerOption.assets.filter(file => !isExcluded(file)),
+  '/',
+  'https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0-beta.2/superhero/bootstrap.min.css',
+  'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+];
+
+const updateCache = (request, response) =>
+  caches.open(cacheName).then(cache => cache.put(request, response));
+
+const fetchFromNetwork = request =>
+  new Promise((resolve, reject) => {
+    fetch(request).then(response => {
+      if (!isExcluded(request.url) && response) {
+        updateCache(request, response.clone()).then(() => resolve(response));
+      } else {
+        resolve(response);
+      }
+    }, reject);
+  });
+
+const fetchFromCache = request =>
+  caches.match(request).then(response => response || Promise.reject('failed'));
+
+const preCache = () =>
+  caches.open(cacheName).then(cache => {
+    cache.addAll(filesToCache);
+  });
+
+self.addEventListener('install', event => {
+  event.waitUntil(preCache());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetchFromNetwork(event.request).catch(() => fetchFromCache(event.request))
+  );
+})
